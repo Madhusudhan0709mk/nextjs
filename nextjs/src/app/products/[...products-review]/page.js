3c4093095eefbe754dfd966d909722325d758961
Madhusudhan K <madhumk0709m@gmail.com>
2024-09-12 15:15:56 +0530

    first commit

---
+++ nextjs/src/app/products/[...products-review]/page.js
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <>
+      <h1>this product review page and this a catch all segment</h1>
+    </>
+  )
+}
+
+export default page
---
+++ nextjs/src/app/products/membership/page.js
@@ -0,0 +1,10 @@
+
+const page = () => {
+  return (
+    <>
+      membership
+    </>
+  )
+}
+
+export default page
